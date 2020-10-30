d8391ab214c512ca189962944b657326b807c1ca
Nicolas Costa <nicolascc.uece@gmail.com>
2020-10-30 16:02:28 -0300

    Creating django base with django-extensions and postgres; Creating Makefile.

---
+++ boilerplate/urls.py
@@ -0,0 +1,49 @@
+"""boilerplate URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# django
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
+from django.views.decorators.cache import cache_page
+
+# third party
+from django_js_reverse.views import urls_js
+from two_factor.admin import AdminSiteOTPRequired
+from two_factor.gateways.twilio.urls import urlpatterns as tf_twilio_urls
+
+# project
+from accounts.allauth_urls import urlpatterns as allauth_urls
+from accounts.tf_urls import urlpatterns as tf_urls
+from core import views as core_views
+
+admin.site.__class__ = AdminSiteOTPRequired
+
+
+urlpatterns = [
+    # django
+    path('admin/', admin.site.urls),
+
+    # auth
+    # path('', include(allauth_urls)),
+    # path('', include(tf_urls)),
+    # path('', include(tf_twilio_urls)),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
